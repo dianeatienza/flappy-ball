9d27999d9f86c49889ad00aa1529c0cf19fef6fa
ldcatienza <ldcatienza@gmail.com>
2023-12-05 23:50:32 +0800

    added draft of flappy ball

---
+++ script.js
@@ -0,0 +1,54 @@
+const block = document.getElementById("block");
+const hole = document.getElementById("hole");
+const character = document.getElementById("character");
+const startButton = document.getElementById("startButton");
+let jumping = 0;
+let counter = 0;
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    hole.addEventListener('animationiteration', () => {
+        const random = -((Math.random()*300)+150);
+        hole.style.top = random + "px";
+        counter++;
+    });
+
+    setInterval(() => {
+        const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if (jumping === 0) {
+            character.style.top = (characterTop + 3) + "px";
+        }
+        const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+        const cTop = -(500 - characterTop);
+    
+        console.log("Character:", characterTop, "Block:", blockLeft, "Hole:", holeTop);
+    
+        if (characterTop > 480 || blockLeft < 20 && blockLeft > -50 && (cTop < holeTop || cTop > holeTop + 160)) {
+            alert(`Game over. Score: ${counter - 1}`);
+            character.style.top = 100 + "px";
+            counter = 0;
+        }
+    }, 10);
+    
+
+    document.addEventListener("click", jump);
+
+    function jump() {
+        jumping = 1;
+        let jumpCount = 0;
+        const jumpInterval = setInterval(() => {
+            const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+            if (characterTop > 6 && jumpCount < 15) {
+                character.style.top = (characterTop - 5) + "px";
+            }
+            if (jumpCount > 20) {
+                clearInterval(jumpInterval);
+                jumping = 0;
+                jumpCount = 0;
+            }
+            jumpCount++;
+        }, 10);
+    }
+}
